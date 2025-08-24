a8c7602151e8da0c5c2888b95e1a08781b2d1bc8
Ethicalhat00 <matheen636@gmail.com>
2025-08-24 21:16:33 +0530

    "cimmit"

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -10,7 +10,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-const sendEmail=async ({o,subject,body}) => {
+const sendEmail=async ({to,subject,body}) => {
     const response=await transporter.sendMail({
         from:process.env.SENDER_EMAIL,
         to,
